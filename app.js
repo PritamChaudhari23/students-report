cd68145b52c00ba0cae50d403319bead3b164320
Pritam Chaudhari <pritam.chaudhari@nitorinfotech.com>
2026-09-01 00:39:07 +0530

    New changes

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "css")));
 
 module.exports = app;
